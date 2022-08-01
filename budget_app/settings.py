022305c3864ad58e672322b52f8d1043284a7255
George <georgemiskander@hotmail>
2022-07-31 21:20:52 -0500

    Updated Settings

--- budget_app/settings.py
+++ budget_app/settings.py
@@ -87,10 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'budgetapp',
-        'USER': 'postgres',
-        'PASSWORD':'Northz1?April28',
-        'HOST' : 'database-1.cxarlcnl3jqf.us-east-2.rds.amazonaws.com',
-        'PORT': '5432'
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
+        'HOST' : os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
 
     }
 }
